0e66bd51f26fedcdc3aae490b6835371b0e5c88c
Rouni Gorgees <rounigorgees@Rounis-iMac.local>
2024-09-29 16:08:44 -0400

    add check url secure file

---
+++ check_url_secure.py
@@ -0,0 +1,20 @@
+import certifi
+import requests
+
+
+def is_secure_website(url):
+    try:
+        response = requests.get(url, verify=certifi.where())
+        response.raise_for_status()
+        return response.url.startswith('https://')
+    except requests.exceptions.RequestException as e:
+        return False
+
+
+if __name__ == "__main__":
+    website_url = input("Enter the website URL: ")
+
+    if is_secure_website(website_url):
+        print(f"{website_url} is a secure website.")
+    else:
+        print(f"{website_url} is not a secure website.")
